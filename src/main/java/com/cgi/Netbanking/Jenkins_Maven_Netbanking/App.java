46f28ad60e96fbc44611581b4854af1cc95d548b
Bharath Kumar <bbharath1904@gmail.com>
2017-10-26 10:37:23 +0530

    first commit

---
+++ src/main/java/com/cgi/Netbanking/Jenkins_Maven_Netbanking/App.java
@@ -0,0 +1,13 @@
+package com.cgi.Netbanking.Jenkins_Maven_Netbanking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
